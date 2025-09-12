273e5b2817254338a71eec8d7054c59a0cd554f5
Sultan <bransyahtan@gmail.com>
2025-09-12 15:44:57 +0700

    test lottie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+
+import doctorAnimation from "@/../public/lotties/doctor.json";
+import Lottie from "lottie-react";
+
 export default function Home() {
   return (
-    <div className="">
-      <p className="min-h-screen text-primarygreen font-extrabold">
-        makanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakan
-      </p>{" "}
-      <p className="min-h-screen">
-        makanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakan
-      </p>
-    </div>
+    <main>
+      <section className="flex h-screen items-center justify-center">
+        <Lottie
+          animationData={doctorAnimation}
+          loop
+          style={{ width: 300, height: 300 }}
+        />
+      </section>
+
+      <section className="min-h-screen">
+        <p className="text-primarygreen font-extrabold">
+          makanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakan
+        </p>
+      </section>
+    </main>
   );
 }
