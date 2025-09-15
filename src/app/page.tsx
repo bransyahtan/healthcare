443a6f907693090255c6f649688b398a922a2393
Sultan <bransyahtan@gmail.com>
2025-09-15 11:35:04 +0700

    add forgot password and register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import doctorAnimation from "@/../public/lotties/doctor.json";
-import Lottie from "lottie-react";
-
 export default function Home() {
   return (
     <main>
-      <section className="flex h-screen items-center justify-center">
-        <Lottie
-          animationData={doctorAnimation}
-          loop
-          style={{ width: 300, height: 300 }}
-        />
-      </section>
-
-      <section className="min-h-screen">
-        <p className="text-primarygreen font-extrabold">
-          makanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakanmakan
-        </p>
-      </section>
+      <section></section>
     </main>
   );
 }
